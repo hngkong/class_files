5fddeceda301c748b25f68c8a16b79c93cfcfb63
Kline Charles Lewis <clk17@hood.edu>
2019-04-02 19:45:15 -0400

    Added comments

--- in_class/additional.py
+++ in_class/additional.py
@@ -7,19 +7,24 @@
 
 class MyClass():
 
+    # Initial variable setup
     myVar = "Example"
 
 def main():
 
+    # Instantiate two objects of the MyClass() type
     myobj1 = MyClass()
     myobj2 = MyClass()
 
+    # Print the values of myVar for both objects
     print(myobj1.myVar)
     print(myobj2.myVar)
 
+    # Change the values of myVar for both objects
     myobj1.myVar = "Passed"
     myobj2.myVar = "Failed"
 
+    # Print the values of myVar for both objects again
     print(myobj1.myVar)
     print(myobj2.myVar)
 
