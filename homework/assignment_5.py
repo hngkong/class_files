d9a7734beec4396e706fd4ff8cf94f8da7afac5b
Charles Kline <hngkong@gmail.com>
2019-04-08 20:03:45 -0400

    Setup

---
+++ homework/assignment_5.py
@@ -0,0 +1,14 @@
+# *********************************************************************************************************
+# Class: IT-512 Spring 2019
+# Author: Charles Kline
+# Due Date:
+# Software/Program Reference Number: Homework Assignment 4
+# Software/Program Brief Description: 
+#                                     
+#                                     
+#
+# Ver No.    Name(Initials)  Date           Description
+# =======    ==============  ===========    ===========
+# 1.0        CK              2019/??/02     First Version
+# *********************************************************************************************************
+# Variable Initialization
