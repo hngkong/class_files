38f18b953ebfe424ff0e144fd8892cef5348ec36
Kline Charles Lewis <clk17@hood.edu>
2019-04-23 20:27:22 -0400

    Class work

--- final/blackjack.py
+++ final/blackjack.py
@@ -29,6 +29,9 @@ class Player:
     def newBet(self):
         self.current_bet = float(input("How much would you like to bet? "))
 
+class Cards:
+    deck = [["Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts", "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts","Ace of Diamonds", "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds","Ace of Clubs", "Two of Clubs", "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs","Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades", "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades", "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades", "King of Spades"],[1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10]]
+
 def main():
     print("Welcome!")
     FirstPlayer = Player()
--- in_class/secondtolast.py
+++ in_class/secondtolast.py
@@ -13,5 +13,32 @@
 # Variable Initialization
 
 days_of_week = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"]
+months_of_week = ["Junairy", "Februetheday", "Forward, march", "Ape-rill"]
 
 print("Contents of list:", days_of_week)
+
+for x in days_of_week:
+    print("The day of the week is:", x)\
+
+index = 0
+while index < len(days_of_week):
+    print("Day of the week:", days_of_week[index])
+    index += 1
+
+confusion = days_of_week + months_of_week
+print(confusion)
+
+less_confusion = confusion[4:7]
+print(less_confusion)
+
+days_tuple = ("manday", "twosday", "white wedding", "thor ragnarok", "fried day", "saturnday", "sunny day")
+print(days_tuple)
+
+index = 0
+while index < len(days_tuple):
+    print("Day of the week:", days_tuple[index])
+    index += 1
+
+list_o_numbers = (0,1,2,3,4,5,6,7,8,1000,133,1440000,2)
+
+print("The highest value is: ", max(list_o_numbers), "the lowest is:", min(list_o_numbers))
