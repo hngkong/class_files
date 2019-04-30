52ade412c31fc2e8deee322a6c00d432aaf49b93
Kline Charles Lewis <clk17@hood.edu>
2019-04-30 19:54:37 -0400

    Oh, life goes on... long after the thrill of living is gone.

---
+++ in_class/dictionary.py
@@ -0,0 +1,8 @@
+phonebook = {"Chris":"555-1111", 
+"Jack":555-2222, 
+"Diane":"Two american kids growing up in the heartland."}
+
+print(phonebook["Chris"])
+print(phonebook["Diane"])
+print(phonebook["Jack"])
+print(phonebook)
